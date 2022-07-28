1f536a240e5baf64c2d2359ad1878929913374b8
tymchuk.volodymyr <tymchuk.volodymyr@gmail.com>
2022-07-28 14:48:35 +0300

    AuthorDAO: Java Doc

--- src/main/java/com/tvv/db/dao/AuthorDAO.java
+++ src/main/java/com/tvv/db/dao/AuthorDAO.java
@@ -6,13 +6,40 @@ import com.tvv.service.exception.AppException;
 
 import java.util.Set;
 
+/**
+ * Interface for work with Authors (CRUD)
+ */
 public interface AuthorDAO {
+    /**
+     * Method for create author in DB
+     * @param author entity author
+     * @return result of creation (false/true)
+     * @throws AppException custom exception
+     */
     boolean create(Author author) throws AppException;
 
+    /**
+     * Method find Author from DB by id
+     * @param id long value author id in DB
+     * @return entity author
+     * @throws AppException custom exception
+     */
     Author findById(long id) throws AppException;
 
+    /**
+     * Method for update author in DB
+     * @param author entity Author with new personnel field
+     * @return result
+     * @throws AppException custom exception
+     */
     boolean update(Author author) throws AppException;
 
+    /**
+     * Method for delete author from DB
+     * @param author entity Author
+     * @return result
+     * @throws AppException custom exception
+     */
     boolean delete(Author author) throws AppException;
 
 }
